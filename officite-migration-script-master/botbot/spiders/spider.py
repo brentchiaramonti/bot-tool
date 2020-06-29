90465fe9203f20c65e96c83f9c660714849fd9af
brentchiaramonti <35432244+brentchiaramonti@users.noreply.github.com>
2020-06-29 08:49:16 -0500

    Fixed bug on webmanager to webmanager pull

--- officite-migration-script-master/botbot/spiders/spider.py
+++ officite-migration-script-master/botbot/spiders/spider.py
@@ -97,9 +97,9 @@ class BotBotSpider(scrapy.Spider):
             self.website.main_content_selector = '#content'
             print('Identified a TherapySites legacy site.', flush=True)
             logger_friendly.info('Identified a TherapySites legacy site.')
-        elif response.css('.editable__container--inner'):
+        elif response.css('.wrap__page-content--inner'):
             self.website.brand = 'W'
-            self.website.main_content_selector = '.editable__container--inner .editable__container--inner'
+            self.website.main_content_selector = '.wrap__page-content--inner'
             print('Identified a Webmanager website', flush=True)
             logger_friendly.info('Identified a Webmanager website')
         else:
